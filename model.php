ba9a2b3e1e4351be1b2a36ac40ee99fde5c1da2b
Arman Antonyan <arman.antonyan@softconstruct.local>
2019-11-04 17:20:43 +0400

    '04/11/2019'

--- model.php
+++ model.php
@@ -3,8 +3,19 @@
     include 'querybuilder.php';
     $orm = new  run("localhost","orm","root","");
 
-    var_dump(querybuilder::select('users','name'));
 
+    var_dump(querybuilder::insert());
+//    $k = array(
+//        'name'=>'name',
+//        'lastname'=>'lastname',
+//        'email'=>'email'
+//    );
+//    $v = array(
+//        'name'=>'arman',
+//        'lastname'=>'antonyan',
+//        'email'=>'arman.antonyan12@gmail.com'
+//    );
+//    querybuilder::select('users',$k,$v);
 
 
 ?>
--- querybuilder.php
+++ querybuilder.php
@@ -8,14 +8,25 @@
 
 class querybuilder
 {
-
+       public static function selectdb($dbname){
+                $command = "USE $dbname;";
+                return run::Run($command);
+       }
        public static function select($where,$column = null){
             if(gettype($column) == null){
                 $column = "*";
             }
-            $command = "select $column from $where";
+            $command = "select $column from `$where`;";
             return run::Run($command);
         }
+
+        public static function insert($row,$column){
+                $command = "INSERT INTO users(name,lastname,email) VALUES ('lorenso','lamas','ujextxa@gmail.com');";
+                return run::Run($command);
+        }
+
+
+
 
 
 }
