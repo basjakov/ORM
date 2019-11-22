67f44d4c7cf9f305a7235344ceb7d33e21e92f17
Arman <arman.antonyan12@gmail.com>
2019-11-22 15:34:10 +0400

    Update querybuilder.php

--- querybuilder.php
+++ querybuilder.php
@@ -2,49 +2,72 @@
 /**
  * Created by PhpStorm.
  * User: arman.antonyan
- * Date: 30/10/2019
- * Time: 17:03
+ * Date: 14/11/2019
+ * Time: 16:35
  */
 
-include 'include/where.php';
 
+/*Query builder like user  class in DataMapper php*/
 
+include 'UserMapper.php';
 
-
-final class querybuilder
+class QueryBuilder
 {
 
-       public static function selectdb($dbname){
-                $command = "USE $dbname;";
-                return run::Run($command);
-       }
-       public static function select($where,$column = null){
+    private $sql = [
+        'select' => 'SELECT *',
+        'from' => '',
+        'where' => '',
+        'limit' => ''
+    ];
 
-            if(gettype($column) == null){
-                $column = "*";
-            }
-            $command = "select $column from `$where`;";
-            return run::Run($command);
+    public function select($field)
+    {
+        $this->sql['select'] = 'SELECT ' . $field;
+
+        return $this;
+    }
+
+    public function from($table)
+    {
+        $this->sql['from'] = "FROM `$table`";
+
+        return $this;
+    }
+
+    public function where($key, $condition, $value)
+    {
+        $this->sql['where'] = "WHERE `$key` $condition `$value`";
+
+        return $this;
+    }
+
+    public function limit($value)
+    {
+        $this->sql['limit'] = "LIMIT $value";
+
+        return $this;
+    }
+
+    public function getSql()
+    {
+        $sql = '';
+
+        foreach ($this->sql as $part) {
+            $sql .= $part . ' ';
         }
 
-        public static function insert($table,array $column,array $row){
-
-                $command = "INSERT INTO $table($column) VALUES ($row)";
-
-                return run::Run($command);
-
-        }
-
-
-        public static function findid($table,$id){
-                                $command = "SELECT * FROM $table WHERE 'id' == $id";
-                                return run::Run($command);
-        }
-
-
-
-
-
-
-
+        return $sql;
+    }
 }
+
+
+class QB {
+    public static function __callStatic($name, $args)
+    {
+        return call_user_func_array([new QueryBuilder(), $name], $args);
+    }
+}
+
+
+
